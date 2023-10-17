ef5066e77d7c3fd56a71696e0e172db86226883e
Yashasvi <yashasvivij01@gmail.com>
2023-10-18 04:38:49 +0530

    Done

---
+++ src/Images.js
@@ -0,0 +1,11 @@
+import image1 from "./ImageFolder/image1.png";
+import image2 from "./ImageFolder/image2.jpeg";
+import image3 from "./ImageFolder/image3.jpeg";
+import image4 from "./ImageFolder/image4.png";
+import image5 from "./ImageFolder/image5.png";
+import image6 from "./ImageFolder/image6.jpeg";
+import image7 from "./ImageFolder/image7.jpeg";
+import image8 from "./ImageFolder/image8.png";
+
+const Images = [image1, image2, image3, image4, image5, image6, image7, image8];
+export default Images;
